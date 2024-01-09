41a3e1496249020cc23ea063c129323a704639a2
Márton Kardos <power.up1163@gmail.com>
2024-01-09 13:17:54 +0100

    Changed clone to LFS

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
-pip install datasets huggingface_hub[clie]
+pip install datasets huggingface_hub
 huggingface-cli login
 
 echo "Cloning repository."
-git clone "https://huggingface.co/datasets/EuropeanParliament/Eurovoc"
+git lfs clone "https://huggingface.co/datasets/EuropeanParliament/Eurovoc"
 
 echo "Selecting Scandinavian entries."
 python3 select_scandi.py
